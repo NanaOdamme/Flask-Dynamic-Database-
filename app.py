72a5732b1699cf94d8ab58c09e60a1e74fb32da2
NanaOdamme <eronastyles@gmail.com>
2024-02-27 15:00:25 +0000

    updated

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
+# Define a global variable to store the added books
+library = []
 
-@app.route("/")
-def hello():
-  return "I am a flask app"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/library')
+def library_page():
+    return render_template('library.html', library=library)
+
+
+@app.route('/add_to_library/<book_title>')
+def add_to_library(book_title):
+    global library
+    if book_title not in library:
+        library.append(book_title)
+    return render_template('index.html')
 
 
 print(__name__)
